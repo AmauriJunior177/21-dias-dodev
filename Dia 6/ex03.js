05a4dad98570f62be6272ae857ffd7c6236cb374
Amauri Junior <mendesamauri177@gmail.com>
2024-12-19 23:48:54 -0300

    Exercício contando até o número escolhido (For)

---
+++ Dia 6/ex03.js
@@ -0,0 +1,19 @@
+let num = Number(0)
+
+
+for(let cont = 50; cont >= num; cont -= 5 ){
+    console.log(cont)
+}
+
+
+
+
+
+
+
+
+
+
+
+//3 - Agora faça com que o algoritmo do exercício anterior exiba a mesma contagem mas na ordem inversa.
+// Ex.: 50 45 40 35 30 25 … 5 0 Fim!
