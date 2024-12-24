037024ba9100c785bb9a5a860bfb0b5bc28ea3ae
Amauri Junior <mendesamauri177@gmail.com>
2024-12-24 20:35:25 -0300

    Primeiros passos (com erro) da calculadora em JS. FEliz natal!!!

---
+++ Calculadora entregável #1/script.js
@@ -0,0 +1,14 @@
+
+
+function calculadora(num1, num2, operacao) {
+    if (operacao == '+'){
+        return num1 + num2
+    }else if (operacao == '-'){
+        return num1 - num2
+    }else if (operacao == '*'){
+        return num1 * num2
+    }else if (operacao == '/'){
+        return num1 / num2 
+    };
+
+calculadora(524,555,'/')}
