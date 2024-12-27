886234d16d1f52013417497e07afd3ec27ec8302
Amauri Junior <mendesamauri177@gmail.com>
2024-12-26 22:16:46 -0300

    Ajustes da calculadora para entrega final

--- Calculadora entregável #1/script.js
+++ Calculadora entregável #1/script.js
@@ -1,14 +1,16 @@
+operacao = prompt('Digite um operador')
+let num1 = parseInt(prompt('Digite um número '))
+let num2 = parseInt(prompt('Digite outro número '))
 
-
-function calculadora(num1, num2, operacao) {
+function calculadora() {
     if (operacao == '+'){
-        return num1 + num2
+        return console.log(num1 + num2)
     }else if (operacao == '-'){
-        return num1 - num2
+        return console.log(num1 - num2)
     }else if (operacao == '*'){
-        return num1 * num2
+        return console.log(num1 * num2)
     }else if (operacao == '/'){
-        return num1 / num2 
+        return console.log(num1 / num2) 
     };
-
-calculadora(524,555,'/')}
+}
+calculadora()
